160207cf92c81371f01f2417101a91d832341972
kumudug <kumudug1@gmail.com>
2023-01-31 19:17:26 -0800

    ex2.2_polymorphic-function

--- src/test/kotlin/chapter2/exercises/ex2/listing.kt
+++ src/test/kotlin/chapter2/exercises/ex2/listing.kt
@@ -12,40 +12,46 @@ val <T> List<T>.tail: List<T>
 val <T> List<T>.head: T
     get() = first()
 
-fun <A> isSorted(aa: List<A>, order: (A, A) -> Boolean): Boolean =
-
-    SOLUTION_HERE()
+fun <A> isSorted(aa: List<A>, order: (A, A) -> Boolean): Boolean {
+    tailrec fun loop(n: A, nl: List<A>): Boolean =
+        when {
+            nl.isEmpty() -> true
+            (!order(n, nl.first())) -> false
+            else -> loop(nl.head, nl.tail)
+        }
+    return aa.isEmpty() || loop(aa.head, aa.tail)
+}
 // end::init[]
 
 //TODO: Enable tests by removing `!` prefix
 class Exercise2 : WordSpec({
 
     "isSorted" should {
-        """!detect ordering of a list of correctly ordered Ints based
+        """detect ordering of a list of correctly ordered Ints based
             on an ordering HOF""" {
             isSorted(
                 persistentListOf(1, 2, 3)
             ) { a, b -> b > a } shouldBe true
         }
-        """!detect ordering of a list of incorrectly ordered Ints
+        """detect ordering of a list of incorrectly ordered Ints
             based on an ordering HOF""" {
             isSorted(
                 persistentListOf(1, 3, 2)
             ) { a, b -> b > a } shouldBe false
         }
-        """!verify ordering of a list of correctly ordered Strings
+        """verify ordering of a list of correctly ordered Strings
             based on an ordering HOF""" {
             isSorted(
                 persistentListOf("a", "b", "c")
             ) { a, b -> b > a } shouldBe true
         }
-        """!verify ordering of a list of incorrectly ordered Strings
+        """verify ordering of a list of incorrectly ordered Strings
             based on an ordering HOF""" {
             isSorted(
                 persistentListOf("a", "z", "w")
             ) { a, b -> b > a } shouldBe false
         }
-        "!return true for an empty list" {
+        "return true for an empty list" {
             isSorted(persistentListOf<Int>()) { a, b ->
                 b > a
             } shouldBe true
